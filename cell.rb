e4ce815d76e2362e1ddd0bf4ce99631bca4301a2
Angela Woods <woods2007@gmail.com>
2015-01-03 18:00:50 -0500

    Made the first batch of files.

---
+++ cell.rb
@@ -0,0 +1,14 @@
+module TicTacToe
+	class Cell
+		attr_accessor :value
+		def initialize(value = "")
+			@value = value
+		end
+	end
+end
+
+=begin
+
+This module keeps track of the cell's value, whether it's an X or an O
+
+=end
---
+++ tic_tac_toe/spec/cell_spec.rb
@@ -0,0 +1,8 @@
+require "spec_helper"
+
+module TicTacToe
+	describe Cell do
+		# tests will be added here
+	end
+	
+end
