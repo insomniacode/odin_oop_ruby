15a153bc6f7bba2ef7dbcd142076f34801f5d44e
Angela Woods <woods2007@gmail.com>
2015-01-05 22:19:46 -0500

    RSpec file for core extensions done for the night.

--- tic_tac_toe/lib/core_extensions.rb
+++ tic_tac_toe/lib/core_extensions.rb
@@ -2,4 +2,17 @@ class Array
 	def all_empty?
 		self.all? {|element| element.to_s.empty?}
 	end	
+
+	def all_same?
+		self.all? { |element| element == self[0] }		
+	end
+
+	def any_empty?
+		self.any? { |element| elements.to_s.empty? }
+	end
+
+	def none_empty?
+		!any_empty?
+	end
+
 end
--- tic_tac_toe/spec/core_extensions_spec.rb
+++ tic_tac_toe/spec/core_extensions_spec.rb
@@ -14,4 +14,48 @@ describe Array do
 			expect([].all_empty?).to be_true
 		end
 	end
+
+	context "#all_same?" do
+		it "returns true if all elements of the Array are the same" do
+			expect(["A", "A", "A"].all_same?).to be_true
+		end
+
+		it "returns false if some of the Array elements are not the same" do
+			expect(["", 1, "", Object.new, :a].all_same?).to be_false
+		end
+
+		it "returns true for an empty Array" do
+			expect([].all_same?).to be_true			
+		end
+
+	end
+
+	context "any_empty?" do
+			it "returns true if any of the elements in the Array are empty" do 
+				expect([].any_empty?).to be_true				
+			end
+
+			it "returns false if none of the elements in the Array are empty" do
+				expect(["A", "A", "A"].any_empty?).to be_false
+			end
+
+
+		end
+
+	end
+
+	context "#none_empty?" do
+		it "returns false if all elements of the Array are empty" do
+			expect(["", 1, ""].all_empty?).to be_false
+		end
+
+		it "returns true if some of the Array elements are not empty" do
+			expect(["", 1, ""], Object.new, :a).to be_true
+		end
+
+		it "returns false for an empty Array" do
+			expect([].all_empty?).to be_false
+		end
+	end
+
 end
